0d0262feed4899e4461af7588e296313242464e1
pinghu <ping.hu@gmail.com>
2019-05-16 17:10:08 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,12 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+#export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+#export_file_name = 'export.pkl'
+##bblink
+#export_file_url = 'https://drive.google.com/uc?export=download&id=13TnakwFgU5tDwNnhi7OgCdZVBmnzRLsJ'
+#export_file_name = 'BB051519Model.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1LUYVu3rSCxrVclu0cR_10KwHeAocp5aZ'
 export_file_name = 'export.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
